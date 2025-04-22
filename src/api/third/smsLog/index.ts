56810966e2c17ad407e8a3215b193f1342543f43
wengxs <754543850@qq.com>
2025-04-22 23:26:07 +0800

    update third sms api url

--- src/api/third/smsLog/index.ts
+++ src/api/third/smsLog/index.ts
@@ -4,10 +4,10 @@ import { SmsLogQuery, SmsLogVO } from "./types";
 
 /** 获取分页列表 */
 export const listSmsLog = (queryParams: SmsLogQuery): AxiosPromise<PageResult<SmsLogVO[]>> =>
-  request.get('/third/smsLog/list', { params: queryParams });
+  request.get('/third-api/sms/log/list', { params: queryParams });
 
 /** 获取详情 */
-export const getSmsLog = (id: number): AxiosPromise<SmsLogVO> => request.get(`/third/smsLog/${id}`);
+export const getSmsLog = (id: number): AxiosPromise<SmsLogVO> => request.get(`/third-api/sms/log/${id}`);
 
 /** 删除 */
-export const delSmsLog = (ids: number[]) => request.delete(`/third/smsLog/${ids}`);
+export const delSmsLog = (ids: number[]) => request.delete(`/third-api/sms/log/${ids}`);
